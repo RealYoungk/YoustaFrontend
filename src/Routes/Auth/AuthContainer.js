29b9972990cb5a615fd2d95adc9831055774a597
realyoungk <9725394@naver.com>
2020-08-11 14:52:36 +0900

    #5.7 createAccount Mutation part Three

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -10,11 +10,12 @@ export default () => {
   const username = useInput("");
   const firstName = useInput("");
   const lastName = useInput("");
-  const email = useInput("itnico.las.me@gmail.com");
-  const requestSecretMutation = useMutation(LOG_IN, {
+  const secret = useInput("");
+  const email = useInput("9725394@naver.com");
+  const [requestSecretMutation] = useMutation(LOG_IN, {
     variables: { email: email.value },
   });
-  const createAccountMutation = useMutation(CREATE_ACCOUNT, {
+  const [createAccountMutation] = useMutation(CREATE_ACCOUNT, {
     variables: {
       email: email.value,
       username: username.value,
@@ -28,10 +29,16 @@ export default () => {
     if (action === "logIn") {
       if (email.value !== "") {
         try {
-          const { requestSecret } = await requestSecretMutation();
+          const {
+            data: { requestSecret },
+          } = await requestSecretMutation();
+          console.log(requestSecret);
           if (!requestSecret) {
             toast.error("You dont have an account yet, create one");
             setTimeout(() => setAction("signUp"), 3000);
+          } else {
+            toast.success("Check your inbox for your login secret");
+            setAction("confirm");
           }
         } catch {
           toast.error("Can't request secret, try again");
@@ -47,7 +54,9 @@ export default () => {
         lastName.value !== ""
       ) {
         try {
-          const { createAccount } = await createAccountMutation();
+          const {
+            data: { createAccount },
+          } = await createAccountMutation();
           if (!createAccount) {
             toast.error("Can't create account");
           } else {
@@ -71,6 +80,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
+      secret={secret}
       onSubmit={onSubmit}
     />
   );
