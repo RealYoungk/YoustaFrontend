abeb8c30a9f0a1e100e295c86d69644ab2ab6438
realyoungk <9725394@naver.com>
2020-08-26 15:33:25 +0900

    #9.0 Profile Screen part One

--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -4,6 +4,7 @@ import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment as CommentIcon } from "../Icons";
+import { Link } from "react-router-dom";
 
 const Post = styled.div`
   ${(props) => props.theme.whiteBox};
@@ -103,6 +104,13 @@ const Comment = styled.li`
   }
 `;
 
+const Caption = styled.div`
+  margin: 15px 0px;
+  span {
+    margin-right: 5px;
+  }
+`;
+
 export default ({
   user: { username, avatar },
   location,
@@ -116,12 +124,15 @@ export default ({
   onKeyPress,
   comments,
   selfComments,
+  caption,
 }) => (
   <Post>
     <Header>
       <Avatar size="sm" url={avatar} />
       <UserColumn>
-        <FatText text={username} />
+        <Link to={`/${username}`}>
+          <FatText text={username} />
+        </Link>
         <Location>{location}</Location>
       </UserColumn>
     </Header>
@@ -131,6 +142,7 @@ export default ({
           <File key={file.id} src={file.url} showing={index === currentItem} />
         ))}
     </Files>
+
     <Meta>
       <Buttons>
         <Button onClick={toggleLike}>{isLiked ? <HeartFull /> : <HeartEmpty />}</Button>
@@ -139,6 +151,10 @@ export default ({
         </Button>
       </Buttons>
       <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
+      <Caption>
+        <FatText text={username} />
+        {caption}
+      </Caption>
       {comments && (
         <Comments>
           {comments.map((comment) => (
