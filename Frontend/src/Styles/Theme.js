89920c4ee1b3028e9090aa82b0e17f8d958d8af6
realyoungk <9725394@naver.com>
2020-09-15 18:47:12 +0900

    initFrontend

---
+++ Frontend/src/Styles/Theme.js
@@ -0,0 +1,19 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+
+export default {
+  maxWidth: "935px",
+  bgColor: "#FAFAFA",
+  balckColor: "#262626",
+  darkGray: "#999",
+  lightGray: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `border:${BOX_BORDER};
+             border-raidus:${BORDER_RADIUS};
+             background-color:white;
+            `,
+};
